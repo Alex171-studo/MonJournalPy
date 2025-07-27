6829d265bf17a746eb74b8413cf9bcbc878b64df
Alex171-studo <godwillaguemonbg@gmail.com>
2025-07-28 00:38:50 +0100

    Nouveau push

--- config.py
+++ config.py
@@ -7,11 +7,20 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.secret_key= "aodnciiz@lslk17226782!!!llsjj<<>_i+"
+
+app.secret_key = os.getenv('FLASK_SECRET_KEY', 'default_secret_key')  
 
 app.permanent_session_lifetime = timedelta(days=30)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'instance','users.sqlite3')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+    'DATABASE_URL', 
+    'sqlite:///' + os.path.join(basedir, 'instance', 'users.sqlite3')  
+)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+
+
+
